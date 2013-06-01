dd45b5830cd67c59d2ae0ec565dad6bbf5165207
Joseph Baugher <jbaughe2@jhmi.edu>
2013-06-01 04:21:46 -0300

    Update triPOD_graphics.R

--- triPOD_graphics.R
+++ triPOD_graphics.R
@@ -1,20 +1,26 @@
 #!/usr/bin/Rscript
 
-#triPOD - detection of chromosomal abnormalities using parent-of-origin-based analyses.
-#triPOD was designed and implemented by Joseph D. Baugher. Email:jbaughe2(at)jhmi.edu.
-#This file is part of triPOD.
-#triPOD is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+############################################################################### 
+#
+# The triPOD software detects chromosomal abnormalities using 
+# the Parent-of-Origin-based Detection (POD) method.
+# Author and maintainer: Joseph D. Baugher. 
+# Email:jbaughe2(at)jhmi.edu.
+#
+# This file is part of the triPOD software.
+# triPOD is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+# triPOD is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+#   along with triPOD.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
-#triPOD is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with triPOD.  If not, see <http://www.gnu.org/licenses/>.
 
 library(shape)
 library(TTR)
